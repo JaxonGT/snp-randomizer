ef64211691a646a66c3d1072949dc57bbe7e2b9d
rjackson <rjackson@MD-62451>
2019-08-16 23:02:25 -0400

    need to add directory for input and output files; need to modify character array to avoid using the same character multiple times

---
+++ stemloop-randomizer/stemloop_randomizer.py
@@ -0,0 +1,38 @@
+import os, sys
+import random
+
+path = "."
+maxNum = 60
+
+for file in os.listdir(path):
+    if file.endswith(".txt"):
+
+        lines = []
+        headers = []
+        chars = []
+
+        f = open(file, "r")
+        for line in f:
+            if line.startswith(">"):
+                headers.append(line)
+            else:
+                lines.append(line)
+
+        f.close()
+
+        for i in range(maxNum):
+            chars = []
+            for j in range(len(lines)):
+                chars.append("")
+                for k in range(i+1):
+                    rand = random.randrange(0,len(lines),1)
+                    chars[j] = chars[j] + lines[j][rand]
+                chars[j] = chars[j] + "\n"
+
+            f = open(file.replace(".txt", "_" + str(i+1) + ".txt"), "w")
+
+            for k in range(len(headers)):
+                f.write(headers[k])
+                f.write(chars[k])
+
+            f.close()
